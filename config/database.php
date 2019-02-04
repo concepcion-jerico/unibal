906dfc53752a8eb9246c3e9cb67d9bfc3cd1c051
concepcion-jerico <concepcion.jerico@gmail.com>
2019-02-04 15:37:24 +0800

    database

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'sql12277275'),
             'username' => env('DB_USERNAME', 'sql12277275'),
-            'password' => env('DB_PASSWORD', '@Jpc8202371'),
+            'password' => env('DB_PASSWORD', 'uDsmeQhjPe'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
